39dc62d0c605c95df436e59bd97aead75558e01c
pawan suthar <pawansuthar15372097023627153@gmail.com>
2023-04-24 20:58:36 +0530

    some changes

--- app/admin.py
+++ app/admin.py
@@ -1,14 +1,10 @@
 from django.contrib import admin
 from.models import *
 
-
-
 # admin customization
-
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('image_show','title','url','date')
     search_fields = ('title',)
-
 
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title','url')
@@ -16,13 +12,11 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('cat',)
     list_per_page = 30
 
-
 #  if i cnage admin them then tinymce wala chalane k liye
 
     # class Media:
     #     js = ('js/script.js',)
 
-
-# Register your models here.
+# Registr your models here.
 admin.site.register(Category, CategoryAdmin)
 admin.site.register(Post,PostAdmin)
